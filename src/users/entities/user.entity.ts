8c804e762ca372091372b3e33519973ddb41ed2b
paczaj <marcinpaczkowski@icloud.com>
2024-09-29 13:13:45 +0200

    Create User entity with initial fields
    Added the User entity to the codebase, defining essential fields such as id, uuid, personal details, and authentication-related attributes. Implemented relationships with UserRole and Site entities, and included timestamps for record creation and updates. This lays the groundwork for user management in the application.

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,64 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  ManyToMany,
+  JoinTable,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { UserRole } from './user-role.entity';
+import { Site } from './site.entity';
+import { uuidv7 } from 'uuidv7';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: 'varchar',
+  })
+  uuid: string = uuidv7();
+
+  @Column({ nullable: false })
+  first_name: string;
+
+  @Column({ nullable: false })
+  last_name: string;
+
+  @Column({ nullable: false })
+  email: string;
+
+  @Column({ nullable: true })
+  phone: string;
+
+  @Column({ nullable: false, select: false })
+  password_hash: string;
+
+  @Column({ default: true })
+  is_init_password: boolean;
+
+  @ManyToOne(() => UserRole, (role) => role.id)
+  role: UserRole;
+
+  @ManyToOne(() => Site, (site) => site.id)
+  default_site: Site;
+
+  @Column({ default: true })
+  is_active: boolean;
+
+  @ManyToMany(() => Site)
+  @JoinTable()
+  other_sites: Site[];
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @ManyToOne(() => User, (user) => user.id)
+  public creator?: number;
+}
