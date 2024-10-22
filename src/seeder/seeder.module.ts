a4ec76176bee76843b01fb2fb87793ed931a92de
paczaj <marcinpaczkowski@icloud.com>
2024-10-22 23:03:10 +0200

    Add FakeUserService to seeder module
    Include the FakeUserService to the providers array in SeederModule. This ensures the seeder module has access to fake user data functionalities.

--- src/seeder/seeder.module.ts
+++ src/seeder/seeder.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SeederService } from './seeder.service';
 import { SeederController } from './seeder.controller';
+import { FakeUserService } from './services/fake.user.service';
 
 @Module({
-  providers: [SeederService],
-  controllers: [SeederController]
+  providers: [SeederService, FakeUserService],
+  controllers: [SeederController],
 })
 export class SeederModule {}
