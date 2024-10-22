1f9d6914e4e6cc25fba007fae9717587b18d3068
paczaj <marcinpaczkowski@icloud.com>
2024-10-22 23:03:00 +0200

    Add database seeding endpoint to SeederController
    Implemented a new GET endpoint in SeederController to trigger database seeding. This includes proper HTTP status code handling and integration with SeederService for executing the seeding process.

--- src/seeder/seeder.controller.ts
+++ src/seeder/seeder.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpCode } from '@nestjs/common';
+import { SeederService } from './seeder.service';
 
 @Controller('seeder')
-export class SeederController {}
+export class SeederController {
+  constructor(private seederService: SeederService) {}
+  @HttpCode(200)
+  @Get('')
+  seedDatabase() {
+    return this.seederService.seedDatabaseData();
+  }
+}
