fb7ad3ed0f1197f17a552f9a6c7d9b8a5be3543a
paczaj <marcinpaczkowski@icloud.com>
2024-10-22 23:02:38 +0200

    Add fake site data for seeding
    Introduced a new file `fake.site.ts` containing an array of fake site objects for seeding purposes. Each site object includes details such as id, name, uuid, address, and description. This will help in testing and development by providing mock data.

---
+++ src/seeder/services/fake.site.ts
@@ -0,0 +1,31 @@
+export const fakeSites = [
+  {
+    id: 1,
+    name: 'WAW01',
+    uuid: 'b438b6fa-765b-4706-8b22-88adb9b5534a',
+    address: 'pl. Książek 43a',
+    description: `Debitis hic blanditiis error dicta enim. Pariatur non iusto doloribus totam iste. Assumenda unde corrupti.Aliquam commodi dolorum vero praesentium. Cupiditate error
+    laborum quibusdam veritatis optio velit sequi. Quasi officiis similique nam aut perspiciatis quas.Sed quidem ad harum. Laboriosam recusandae quibusdam corporis doloremque aliquam al
+    iquid. Repellat ullam optio provident harum sed aliquid adipisci.Laudantium est beatae explicabo ad.`,
+  },
+  {
+    id: 2,
+    name: 'WAW02',
+    uuid: 'e348ef49-c6b2-49df-8163-7814e97cb0bd',
+    address: 'szosa Janowski 61',
+    description: `Officiis quidem reprehenderit iure cumque molestias veritatis cupiditate. Quis recusandae nobis sint nobis inventore nulla possimus eveniet. Inventore odit aliquid 
+    vel.Sunt nesciunt placeat nihil placeat consequatur quos. Occaecati neque distinctio voluptas est voluptatem culpa voluptatem soluta molestiae. Atque ab mollitia.Impedit sint archite
+    cto rerum id aperiam minus totam vel quisquam. Iusto qui ad quibusdam ratione commodi. Adipisci voluptates repudiandae et quam blanditiis enim aperiam adipisci quasi.Placeat debitis doloremque magnam. Atque error consequatur odio doloribus voluptas quasi quam delectus nostrum. 
+    Quod neque reprehenderit distinctio ea ratione reiciendis mollitia pariatur.`,
+  },
+  {
+    id: 3,
+    name: 'WAW03',
+    uuid: '0481f353-134a-4450-a582-94a0e02c0bfd',
+    address: 'wyspa Książek 4/9',
+    description: `Hic repellendus quibusdam sunt. Aut nam consequatur veritatis amet vero nemo. Excepturi amet sint id magnam autem voluptate.Cupiditate tenetur quas blanditiis alias
+    iusto recusandae nesciunt eaque ea. Nulla sit repudiandae dolorum saepe. Placeat quis exercitationem laboriosam eligendi ipsam velit.Eveniet exercitationem culpa tempora neque quis
+    minima accusamus possimus expedita. Autem velit dicta magnam fugit et unde nemo molestias ratione. Ipsum incidunt soluta quas veniam atque quidem voluptates.Consectetur soluta quo nesciunt possimus quam quo. Veniam a animi impedit. Neque nihil placeat laborum consequatur veritatis exercitationem hic.'
+  `,
+  },
+];
