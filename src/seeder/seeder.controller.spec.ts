a208e9b4e4f5ce6e9d58b26ec7fe7a0a7fb59c4c
paczaj <marcinpaczkowski@icloud.com>
2024-10-25 09:12:34 +0200

    Add unit tests for SeederService
    Implemented comprehensive unit tests for `SeederService` including methods to seed and truncate database tables. Introduced mocks for necessary dependencies and validated method calls to ensure correct functionality.

--- src/seeder/seeder.controller.spec.ts
+++ src/seeder/seeder.controller.spec.ts
@@ -1,18 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SeederController } from './seeder.controller';
+import { SeederService } from './seeder.service';
 
 describe('SeederController', () => {
-  let controller: SeederController;
+  let seederController: SeederController;
+  let seederService: SeederService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SeederController],
+      providers: [
+        { provide: SeederService, useValue: { seedDatabaseData: jest.fn() } },
+      ],
     }).compile();
 
-    controller = module.get<SeederController>(SeederController);
+    seederController = module.get<SeederController>(SeederController);
+    seederService = module.get<SeederService>(SeederService);
   });
-
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(seederController).toBeDefined();
   });
 });
