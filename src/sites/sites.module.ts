9196e99cd210b92f19583682e77b3f06e07cdec2
paczaj <marcinpaczkowski@icloud.com>
2024-10-25 11:17:24 +0200

    Add TypeOrmModule to SitesModule
    This change includes the TypeOrmModule to the SitesModule, enabling database interaction for the Site entity. This will facilitate the integration of TypeORM functionalities within the SitesModule, enhancing its data management capabilities.

--- src/sites/sites.module.ts
+++ src/sites/sites.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SitesService } from './sites.service';
 import { SitesController } from './sites.controller';
+import { Site } from '../entities/site.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   controllers: [SitesController],
   providers: [SitesService],
+  imports: [TypeOrmModule.forFeature([Site])],
 })
 export class SitesModule {}
