500853f520e9d54d5e877f78b7dd2bc32b08b697
paczaj <marcinpaczkowski@icloud.com>
2024-10-25 11:17:35 +0200

    Refactor SitesService to use TypeORM repository
    Updated the SitesService to use TypeORM repository for database operations. Replaced in-memory placeholders with actual repository methods for finding sites and added dependency injection for the Site entity.

--- src/sites/sites.service.ts
+++ src/sites/sites.service.ts
@@ -1,19 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { CreateSiteDto } from './dto/create-site.dto';
 import { UpdateSiteDto } from './dto/update-site.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Site } from '../entities/site.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class SitesService {
+  constructor(
+    @InjectRepository(Site) private readonly siteRepository: Repository<Site>,
+  ) {}
+
   create(createSiteDto: CreateSiteDto) {
     return 'This action adds a new site';
   }
 
   findAll() {
-    return `This action returns all sites`;
+    return this.siteRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} site`;
+  async findOne(uuid: string) {
+    console.log(uuid);
+    const result = await this.siteRepository.findOne({
+      where: {
+        uuid: uuid,
+      },
+    });
+    console.log(result);
+    return result;
   }
 
   update(id: number, updateSiteDto: UpdateSiteDto) {
