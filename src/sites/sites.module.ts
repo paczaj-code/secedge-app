6bb47dd0f0c7284601b733771d78546e4d2d9354
paczaj <marcinpaczkowski@icloud.com>
2024-11-28 12:54:41 +0100

    Add BaseDatabaseService to SitesModule providers
    This change integrates BaseDatabaseService into the SitesModule by including it in the providers array. This enhances the module's functionality by leveraging shared database services, promoting code reuse and cohesion within the application.

--- src/sites/sites.module.ts
+++ src/sites/sites.module.ts
@@ -3,10 +3,11 @@ import { SitesService } from './sites.service';
 import { SitesController } from './sites.controller';
 import { Site } from '../entities/site.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { BaseDatabaseService } from '../generic/baseDatabase/baseDatabase.service';
 
 @Module({
   controllers: [SitesController],
-  providers: [SitesService],
+  providers: [SitesService, BaseDatabaseService],
   imports: [TypeOrmModule.forFeature([Site])],
 })
 export class SitesModule {}
