5dbbea95bbcd9475b4f4dd9cf9af187ea694536e
paczaj <marcinpaczkowski@icloud.com>
2024-11-28 12:53:34 +0100

    Add documentation for SeederController
    Introduce a comment block to describe the purpose of the SeederController. This addition clarifies its role in seeding the database during development or testing by utilizing the SeederService.

--- src/seeder/seeder.controller.ts
+++ src/seeder/seeder.controller.ts
@@ -1,6 +1,13 @@
 import { Controller, Get, HttpCode } from '@nestjs/common';
 import { SeederService } from './seeder.service';
 
+/**
+ * Controller responsible for seeding the database.
+ *
+ * This controller provides an endpoint to seed initial data into the database
+ * using the SeederService. It is typically used during development or testing
+ * to initialize the database with a predefined set of data.
+ */
 @Controller('seeder')
 export class SeederController {
   constructor(private seederService: SeederService) {}
