609d6f49531ad2292400f8183b0f6880e15d9b1c
paczaj <marcinpaczkowski@icloud.com>
2024-11-28 12:52:06 +0100

    Refactor e2e tests to use direct DataSource connection
    Replaced the Nest application bootstrapping and entity manager with a direct TypeORM DataSource connection for test setup. This change simplifies the test structure by removing unnecessary service dependencies and directly querying the database. Adjusted test cases to use the new data source for database queries and connection management.

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,49 +1,39 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from '../src/app.module';
-import { FakeUserService } from '../src/seeder/services/fake.user.service';
-import { User } from '../src/entities/user.entity';
-import { Connection, EntityManager } from 'typeorm';
+import { DataSource, DataSourceOptions } from 'typeorm';
 import ormConfig from '../src/config/orm.config';
-import { DataSource } from 'typeorm';
-import { Site } from '../src/entities/site.entity';
-import { fakeSites } from '../src/seeder/services/fake.site';
-import { SeederService } from '../src/seeder/seeder.service';
+
+const dataSource = new DataSource(ormConfig() as DataSourceOptions);
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication;
-  let em: EntityManager;
-  let ss: SeederService;
   beforeAll(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
-
-    app = moduleFixture.createNestApplication();
-    await app.init();
-    em = app.get(EntityManager);
-    ss = app.get(SeederService);
-    const fakeUserService = new FakeUserService();
-    const users = await fakeUserService.generateFakeUsers(22);
-    // console.log(users);
-    await em.insert(Site, fakeSites);
-    await em.insert(User, users);
+    await dataSource.initialize();
   });
-
   afterAll(async () => {
-    await ss.truncateTables();
+    await dataSource.destroy();
   });
 
-  // beforeEach(async () => {});
+  it('/ (GET all sites)', async () => {
+    const allSites = await dataSource.manager.query(
+      'SELECT * FROM sites ORDER BY name ',
+    );
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer()).get('/api').expect(404);
-    // .expect({ message: 'Hello secedge-app' });
+    const response = await request(globalThis.APP.getHttpServer()).get(
+      '/sites',
+    );
+    expect(response.status).toBe(200);
+    expect(response.body.length).toBe(3);
+    expect(response.body[0].name).toBe('WAW01');
+    expect(response.body[0].uuid).toBeDefined();
+    expect(response.body[0].name).toEqual(allSites[0].name);
+    expect(response.body[0].uuid).toEqual(allSites[0].uuid);
   });
-  // it('/ (GET2)', async () => {
-  //   const result = await request(app.getHttpServer()).get('/api/2');
-  //   expect(result.status).toBe(200);
-  //   expect(result.body.message).toEqual('message');
-  // });
+
+  describe('some', () => {
+    it('/ (GET2)', async () => {
+      const result = await request(globalThis.APP.getHttpServer()).get(
+        '/api/2',
+      );
+      expect(result.status).toBe(404);
+    });
+  });
 });
