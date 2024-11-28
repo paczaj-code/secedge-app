0afddea5278a237cdc52ab7703eea66497d079bd
paczaj <marcinpaczkowski@icloud.com>
2024-11-28 12:54:32 +0100

    Refactor and enhance SitesController with async/await
    Converted CRUD methods to use async/await for better asynchronous handling and error management. Added type annotations to improve code readability and maintainability. Introduced custom UUID validation pipe to ensure valid UUID format in route parameters.

--- src/sites/sites.controller.ts
+++ src/sites/sites.controller.ts
@@ -3,41 +3,71 @@ import {
   Get,
   Post,
   Body,
-  Patch,
   Param,
-  Delete,
+  Put,
+  HttpException,
+  ParseUUIDPipe,
 } from '@nestjs/common';
 import { SitesService } from './sites.service';
 import { CreateSiteDto } from './dto/create-site.dto';
 import { UpdateSiteDto } from './dto/update-site.dto';
+import { Site } from '../entities/site.entity';
+import { UuidValidationPipePipe } from '../pipes/uuid-validation-pipe/uuid-validation-pipe.pipe';
 
+/**
+ * SitesController handles all incoming HTTP requests related to site resources.
+ */
 @Controller('sites')
 export class SitesController {
   constructor(private readonly sitesService: SitesService) {}
 
+  /**
+   * Creates a new site.
+   *
+   * @param {CreateSiteDto} createSiteDto - The data transfer object containing the details of the site to be created.
+   * @return {Promise<Site>} A promise that resolves to the created Site object.
+   */
   @Post()
-  create(@Body() createSiteDto: CreateSiteDto) {
-    return this.sitesService.create(createSiteDto);
+  async create(@Body() createSiteDto: CreateSiteDto): Promise<Site> {
+    return await this.sitesService.create(createSiteDto);
   }
 
+  /**
+   * Retrieves all items from the sites service.
+   *
+   * @return {Promise<Site[]>} A promise that resolves to an array of items.
+   */
   @Get()
-  findAll() {
-    return this.sitesService.findAll();
+  async findAll(): Promise<Site[]> {
+    return await this.sitesService.findAll();
   }
 
+  /**
+   * Retrieves a single site based on the provided UUID.
+   *
+   * @param {string} uuid - The unique identifier of the site to be retrieved.
+   * @return {Promise<Site | HttpException>} - A promise that resolves to the site object if found.
+   */
   @Get(':uuid')
-  async findOne(@Param('uuid') uuid: string) {
-    // console.log(uuid);
+  async findOne(
+    @Param('uuid', new UuidValidationPipePipe()) uuid: string,
+  ): Promise<Site | HttpException> {
     return await this.sitesService.findOne(uuid);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateSiteDto: UpdateSiteDto) {
-    return this.sitesService.update(+id, updateSiteDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.sitesService.remove(+id);
+  /**
+   * Updates the site information with the specified UUID using the provided data.
+   *
+   * @param {string} uuid - The unique identifier of the site to be updated.
+   * @param {UpdateSiteDto} updateSiteDto - The data transfer object containing the updated site information.
+   *
+   * @return {Promise<Site>} - A promise that resolves to the updated site information.
+   */
+  @Put(':uuid')
+  async update(
+    @Param('uuid', new UuidValidationPipePipe()) uuid: string,
+    @Body() updateSiteDto: UpdateSiteDto,
+  ): Promise<Site> {
+    return await this.sitesService.update(uuid, updateSiteDto);
   }
 }
