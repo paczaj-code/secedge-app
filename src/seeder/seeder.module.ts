8a91f6511c5a88346ba97a1327cd11558c1d9e3e
paczaj <marcinpaczkowski@icloud.com>
2024-11-28 12:53:43 +0100

    Add documentation for SeederModule structure and purpose
    Document the responsibilities and components of SeederModule, including its services and controller. This provides clarity on how database seeding is managed and the functionalities of SeederService and FakeUserService. The comments aim to guide future developers in understanding and utilizing this module effectively.

--- src/seeder/seeder.module.ts
+++ src/seeder/seeder.module.ts
@@ -3,6 +3,19 @@ import { SeederService } from './seeder.service';
 import { SeederController } from './seeder.controller';
 import { FakeUserService } from './services/fake.user.service';
 
+/**
+ * SeederModule is responsible for setting up the services and controller
+ * related to database seeding. It includes providers for SeederService and
+ * FakeUserService, as well as the SeederController.
+ *
+ * The SeederService handles the logic for seeding the database with fake or
+ * initial data, while the FakeUserService provides the necessary methods to
+ * create and manage fake user data.
+ *
+ * The SeederController exposes endpoints to trigger the seeding process.
+ *
+ * It is intended to be used as part of the application's module system.
+ */
 @Module({
   providers: [SeederService, FakeUserService],
   controllers: [SeederController],
