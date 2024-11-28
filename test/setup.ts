5b3e6799548c35a2e5c949df7a656279969c2dca
paczaj <marcinpaczkowski@icloud.com>
2024-11-28 12:54:06 +0100

    Revamp test setup with NestJS application initialization
    The test setup script has been restructured to initialize a NestJS application as part of the testing environment. This includes setting up dependencies with a TestingModule, starting the application, and performing initial data seeding. These changes ensure a more comprehensive and realistic environment for running tests.

--- test/setup.ts
+++ test/setup.ts
@@ -1,4 +1,33 @@
-module.exports = async () => {
-  // Perform setup tasks, e.g., initialize a database
-  console.log('Global setup');
+import { FakeUserService } from '../src/seeder/services/fake.user.service';
+import { EntityManager } from 'typeorm';
+import { User } from '../src/entities/user.entity';
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppModule } from '../src/app.module';
+import { SeederService } from '../src/seeder/seeder.service';
+import { Site } from '../src/entities/site.entity';
+import { fakeSites } from '../src/seeder/services/fake.site';
+import { INestApplication } from '@nestjs/common';
+
+module.exports = async (
+  _: any,
+  projectConfig: { globals: { APP_PORT: string | number } },
+) => {
+  let app: INestApplication;
+  let entityManager: EntityManager;
+  let seederService: SeederService;
+  const moduleFixture: TestingModule = await Test.createTestingModule({
+    imports: [AppModule],
+  }).compile();
+
+  app = moduleFixture.createNestApplication();
+  await app.init();
+  await app.listen(projectConfig.globals.APP_PORT);
+  globalThis.APP = app;
+  entityManager = globalThis.APP.get(EntityManager);
+  seederService = globalThis.APP.get(SeederService);
+  const fakeUserService = new FakeUserService();
+  const users = await fakeUserService.generateFakeUsers(22);
+  await seederService.truncateTables();
+  await entityManager.insert(Site, fakeSites);
+  await entityManager.insert(User, users);
 };
