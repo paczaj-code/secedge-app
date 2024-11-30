d20e2d401ca92c8a230bfbb823135d815e4068a4
paczaj <marcinpaczkowski@icloud.com>
2024-11-30 11:45:48 +0100

    Remove unused DeepPartial import from users controller
    This import was not being utilized in the `users.controller.ts` file, leading to unnecessary code clutter. Removing it helps in maintaining clean and efficient code.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,7 +10,6 @@ import {
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { DeepPartial } from 'typeorm';
 import { User } from '../entities/user.entity';
 
 @Controller('users')
