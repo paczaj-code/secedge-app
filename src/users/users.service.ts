13d13b55134ac8cd7fe1b73de2fc0deb86933703
paczaj <marcinpaczkowski@icloud.com>
2024-12-02 03:25:31 +0100

    Enhance user service with pagination, filters, and email lookup
    Updated the `findAll` method to support pagination, ordering, and filtering functionality. Introduced a `findUserByEmail` method to locate a user by their email address, throwing an exception if the user is not found. Removed unused dependencies to streamline the service structure.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,47 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { BaseDatabaseService } from '../generic/baseDatabase/baseDatabase.service';
+// import { BaseDatabaseService } from '../generic/baseDatabase/baseDatabase.service';
 import { Site } from '../entities/site.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from '../entities/user.entity';
+import { QueryHelper } from '../generic/query-helper/query-helper';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User) private readonly userRepository: Repository<User>,
-    @InjectRepository(Site) private readonly siteRepository: Repository<Site>,
-    private readonly baseDatabaseService: BaseDatabaseService<typeof User>,
+    // @InjectRepository(Site) private readonly siteRepository: Repository<Site>,
+    // private readonly baseDatabaseService: BaseDatabaseService<typeof User>,
   ) {}
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
 
-  async findAll(): Promise<User[]> {
-    return this.userRepository
+  async findAll(
+    page: number,
+    perPage: number,
+    orderBy: string = 'id',
+    order: 'ASC' | 'DESC' = 'ASC',
+    filters: { [key: string]: string } = {},
+  ): Promise<User[]> {
+    const selectedColumns = [
+      'user.id',
+      'user.uuid',
+      'user.first_name',
+      'user.last_name',
+      'user.email',
+      'user.phone',
+      'user.default_site',
+      'site.name',
+      'site.uuid',
+      'other_sites.name',
+      'other_sites.uuid',
+      'user.created_at',
+      'user.updated_at',
+    ];
+    const queryBuilder = this.userRepository
       .createQueryBuilder('user')
       .leftJoinAndSelect('user.default_site', 'site')
-      .select([
-        'user.id',
-        'user.uuid',
-        'user.first_name',
-        'user.last_name',
-        'user.default_site',
-        'site.name',
-      ])
-      .addSelect((subQuery) => {
-        return subQuery
-          .select(
-            "JSON_AGG(json_build_object('name',site.name, 'uuid',site.uuid))",
-          )
-          .from(Site, 'site')
-          .where(
-            'site.id = ANY(ARRAY(SELECT unnest(user.other_sites)::integer))',
-          );
-      }, 'aa')
-      .orderBy('user.id')
-      .getRawMany();
+      .leftJoinAndSelect('user.other_sites', 'other_sites')
+      .select(selectedColumns)
+      .orderBy(`user.${orderBy}`, order);
+
+    const queryHelper = new QueryHelper<User>(queryBuilder)
+      .applyPagination({ page, perPage })
+      .applyFilters(filters, 'user');
+    return queryHelper.getQueryBuilder().getMany();
   }
 
   async findOne(id: number): Promise<User> {
@@ -76,5 +86,11 @@ export class UsersService {
 
   remove(id: number) {
     return `This action removes a #${id} user`;
+  }
+
+  findUserByEmail(email: string): Promise<User> {
+    const user = this.userRepository.findOne({ where: { email: email } });
+    if (!user) throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+    return user;
   }
 }
