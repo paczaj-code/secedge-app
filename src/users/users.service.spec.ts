1fc9760b72c2f2752778be0550a401ef4a2b23e2
paczaj <marcinpaczkowski@icloud.com>
2024-12-02 03:25:21 +0100

    Implement findUserByEmail test cases in UsersService
    Added test cases in UsersService to verify behavior of findUserByEmail function. These include returning a user if found by email and throwing an HttpException if not found. Mocked the repository's findOne method to simulate these scenarios.

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,35 +1,49 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { Site } from '../entities/site.entity';
-import { EntityManager, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
+import { HttpException, HttpStatus } from '@nestjs/common';
 import { User } from '../entities/user.entity';
-import { BaseDatabaseService } from '../generic/baseDatabase/baseDatabase.service';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let repository: Repository<User>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         UsersService,
-        BaseDatabaseService,
-        EntityManager,
         {
           provide: getRepositoryToken(User),
-          useClass: Repository,
-        },
-        {
-          provide: getRepositoryToken(Site),
           useClass: Repository,
         },
       ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    repository = module.get<Repository<User>>(getRepositoryToken(User));
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('findUserByEmail', () => {
+    it('should return user if found by email', async () => {
+      const email = 'test@example.com';
+      const mockUser = new User();
+      jest.spyOn(repository, 'findOne').mockResolvedValue(mockUser);
+
+      expect(await service.findUserByEmail(email)).toEqual(mockUser);
+    });
+
+    it('should throw HttpException if user is not found', async () => {
+      const email = 'notfound@example.com';
+      jest
+        .spyOn(repository, 'findOne')
+        .mockRejectedValueOnce(
+          new HttpException('User not found', HttpStatus.NOT_FOUND),
+        );
+
+      await expect(service.findUserByEmail(email)).rejects.toThrow(
+        new HttpException('User not found', HttpStatus.NOT_FOUND),
+      );
+    });
   });
 });
