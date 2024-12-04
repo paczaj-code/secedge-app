b431fbacdb803d8f9369ab0ba65c12f86c09601d
paczaj <marcinpaczkowski@icloud.com>
2024-12-04 09:15:11 +0100

    Add HttpExceptionFilter import and comment its usage
    Imported the HttpExceptionFilter but commented out its application in the bootstrap function. This prepares the codebase for future use of custom exception handling without yet modifying the current application behavior.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './exception/http-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({}));
+  // app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
