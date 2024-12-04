3bb434b92998f5555ae1006457227da8e323eee4
paczaj <marcinpaczkowski@icloud.com>
2024-12-04 09:16:21 +0100

    Refactor user queries and update findOne method
    Consolidate selected columns into a class property to simplify and clean up query logic. Modify the findOne method to use 'uuid' as the search parameter instead of 'id' and update response handling to throw a 404 error if the user is not found. This refactoring aims to enhance code readability and maintainability.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,7 +2,6 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 // import { BaseDatabaseService } from '../generic/baseDatabase/baseDatabase.service';
-import { Site } from '../entities/site.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from '../entities/user.entity';
@@ -10,6 +9,22 @@ import { QueryHelper } from '../generic/query-helper/query-helper';
 
 @Injectable()
 export class UsersService {
+  selectedColumns = [
+    'user.id',
+    'user.uuid',
+    'user.first_name',
+    'user.last_name',
+    'user.email',
+    'user.phone',
+    'user.default_site',
+    'site.name',
+    'site.uuid',
+    'other_sites.name',
+    'other_sites.uuid',
+    'user.created_at',
+    'user.updated_at',
+  ];
+
   constructor(
     @InjectRepository(User) private readonly userRepository: Repository<User>,
     // @InjectRepository(Site) private readonly siteRepository: Repository<Site>,
@@ -26,26 +41,11 @@ export class UsersService {
     order: 'ASC' | 'DESC' = 'ASC',
     filters: { [key: string]: string } = {},
   ): Promise<User[]> {
-    const selectedColumns = [
-      'user.id',
-      'user.uuid',
-      'user.first_name',
-      'user.last_name',
-      'user.email',
-      'user.phone',
-      'user.default_site',
-      'site.name',
-      'site.uuid',
-      'other_sites.name',
-      'other_sites.uuid',
-      'user.created_at',
-      'user.updated_at',
-    ];
     const queryBuilder = this.userRepository
       .createQueryBuilder('user')
       .leftJoinAndSelect('user.default_site', 'site')
       .leftJoinAndSelect('user.other_sites', 'other_sites')
-      .select(selectedColumns)
+      .select(this.selectedColumns)
       .orderBy(`user.${orderBy}`, order);
 
     const queryHelper = new QueryHelper<User>(queryBuilder)
@@ -54,30 +54,16 @@ export class UsersService {
     return queryHelper.getQueryBuilder().getMany();
   }
 
-  async findOne(id: number): Promise<User> {
-    return await this.userRepository
+  async findOne(uuid: string) {
+    const user = await this.userRepository
       .createQueryBuilder('user')
+      .where('user.uuid = :uuid', { uuid: uuid })
       .leftJoinAndSelect('user.default_site', 'site')
-      .select([
-        'user.id',
-        'user.uuid',
-        'user.first_name',
-        'user.last_name',
-        'user.default_site',
-        'site.name',
-      ])
-      .addSelect((subQuery) => {
-        return subQuery
-          .select(
-            "JSON_AGG(json_build_object('name',site.name, 'uuid',site.uuid))",
-          )
-          .from(Site, 'site')
-          .where(
-            'site.id = ANY(ARRAY(SELECT unnest(user.other_sites)::integer))',
-          );
-      }, 'aa')
-      .where('user.id = :id', { id: id })
-      .getRawOne();
+      .leftJoinAndSelect('user.other_sites', 'other_sites')
+      .select(this.selectedColumns)
+      .getOne();
+    if (!user) throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+    return user;
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
