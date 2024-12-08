3ad1483a9d9b19ce36e943d399c15f2a1d22d66a
paczaj <marcinpaczkowski@icloud.com>
2024-12-08 08:57:27 +0100

    Refactor authentication token generation logic
    Replaced direct JWT signing with NestJS's JwtService for better integration and maintainability. Centralized token generation logic in `getTokens` method, reducing code redundancy. Enhanced error handling for refresh token verification and added inline documentation to clarify the AuthService responsibilities.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,12 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import * as argon2 from 'argon2';
 import { User } from '../entities/user.entity';
-import * as jwt from 'jsonwebtoken';
 import { UsersService } from '../users/users.service';
 import { LoginDto } from './dto/login.dto';
 import * as fs from 'node:fs';
 import { UserRoles } from '../enums/userRoles';
+import { JwtService } from '@nestjs/jwt';
+import * as process from 'node:process';
 
 interface AccessTokenPayload {
   id: number;
@@ -21,12 +22,19 @@ interface RefreshTokenPayload {
   uuid: string;
 }
 
+/**
+ * AuthService is responsible for handling user authentication and token management operations.
+ * It provides methods for user login, token generation, verification, and refreshing access tokens.
+ */
 @Injectable()
 export class AuthService {
   private readonly privateKey: Buffer;
   private readonly publicKey: Buffer;
 
-  constructor(private userService: UsersService) {
+  constructor(
+    private userService: UsersService,
+    private jwtService: JwtService,
+  ) {
     this.privateKey = fs.readFileSync('private.key');
     this.publicKey = fs.readFileSync('public.pem');
   }
@@ -43,14 +51,15 @@ export class AuthService {
       );
     }
 
-    return {
-      refreshToken: this.generateRefreshToken(user),
-      accessToken: this.generateAccessToken(user),
-    };
+    return this.getTokens(user);
   }
 
-  private buildAccessTokenPayload(user: Partial<User>): AccessTokenPayload {
-    return {
+  async getTokens(user: User) {
+    const refreshTokenPayload: RefreshTokenPayload = {
+      id: user.id,
+      uuid: user.uuid,
+    };
+    const accessTokenPayload: AccessTokenPayload = {
       id: user.id,
       uuid: user.uuid,
       email: user.email,
@@ -58,37 +67,43 @@ export class AuthService {
       firstName: user.first_name,
       lastName: user.last_name,
     };
+    const [refreshToken, accessToken] = await Promise.all([
+      this.jwtService.signAsync(refreshTokenPayload, {
+        secret: process.env.REFRESH_JWT_SECRET,
+        expiresIn: '24h',
+        algorithm: 'HS512',
+      }),
+      this.jwtService.signAsync(accessTokenPayload, {
+        secret: this.privateKey,
+        expiresIn: '5m',
+        algorithm: 'RS512',
+      }),
+    ]);
+
+    return {
+      refreshToken,
+      accessToken,
+    };
   }
 
-  private buildRefreshTokenPayload(
-    user: Pick<User, 'uuid' | 'id'>,
-  ): RefreshTokenPayload {
-    return { id: user.id, uuid: user.uuid };
-  }
+  async refreshAccessToken(refreshToken: string) {
+    try {
+      const payload = await this.jwtService.verifyAsync(refreshToken, {
+        secret: process.env.REFRESH_JWT_SECRET,
+      });
 
-  generateRefreshToken(user: Pick<User, 'uuid' | 'id'>) {
-    const payload = this.buildRefreshTokenPayload(user);
-    const secret = process.env.REFRESH_JWT_SECRET;
-    return jwt.sign(payload, secret, {
-      algorithm: 'HS512',
-      audience: 'secedge',
-      issuer: 'secedge',
-      expiresIn: '13h',
-    });
-  }
+      const now = Date.now() / 1000;
+      const user = await this.userService.findOne(payload.uuid);
+      const newTokens = await this.getTokens(user);
 
-  generateAccessToken(user: Partial<User>) {
-    const payload = this.buildAccessTokenPayload(user);
-    return jwt.sign(payload, this.privateKey, {
-      algorithm: 'RS512',
-      audience: 'secedge',
-      issuer: 'secedge',
-      expiresIn: '5m',
-    });
-  }
+      if ((payload.exp - now) / 3600 > 2) {
+        newTokens.refreshToken = refreshToken;
+      }
 
-  validateAccessToken(token: string) {
-    return jwt.verify(token, this.publicKey, {});
+      return newTokens;
+    } catch (e) {
+      throw new HttpException('Invalid refresh token', HttpStatus.UNAUTHORIZED);
+    }
   }
 
   private async verifyPassword(
@@ -96,5 +111,15 @@ export class AuthService {
     hashedPassword: string,
   ): Promise<boolean> {
     return await argon2.verify(hashedPassword, password);
+  }
+
+  verifyAccessToken(token: string) {
+    return this.jwtService.verify(token, { secret: this.privateKey });
+  }
+
+  verifyRefreshToken(token: string) {
+    return this.jwtService.verify(token, {
+      secret: process.env.REFRESH_JWT_SECRET,
+    });
   }
 }
