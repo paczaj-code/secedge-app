82d6cc0d02d93ec401ccf2aad0906a819a2831c1
paczaj <marcinpaczkowski@icloud.com>
2025-02-15 08:59:19 +0100

    Enable CORS and add compression middleware
    Added CORS configuration to allow any origin, headers, and credentials. Integrated compression middleware to optimize responses and improve performance. These changes enhance the application's scalability and usability.

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './exception/http-exception.filter';
+import * as compression from 'compression';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({}));
+  app.use(compression());
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+    credentials: true,
+  });
   // app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
 }
